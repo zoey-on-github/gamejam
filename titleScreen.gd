1aee0f0ad57bea661eb4132d18998c47ab2cf3a0
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2026-02-06 02:48:06 -0800

    splash texts

--- titleScreen.gd
+++ titleScreen.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	splashTest()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +13,13 @@ func _process(delta: float) -> void:
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
+
+func splashTest():
+	var file = "res://splashes.txt"
+	var list = FileAccess.open(file, FileAccess.READ)
+	var rng = RandomNumberGenerator.new()
+		
+	while not list.eof_reached():
+		%splash.text = "[center][wave freq=10][rainbow freq=0.1 sat=0.5]" + list.get_line() + "[/rainbow][/wave][/center]"
+		if rng.randi() % 10 == 0:
+			return
